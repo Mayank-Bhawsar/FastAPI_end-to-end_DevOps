f3bc705360ecf52142cb4c5afa90ce620c56f342
Mayank-Bhawsar <mayankbhawsar1@gmail.com>
2026-06-23 20:41:37 +0000

    fix: added backend.tf file for s3 backend error

--- infra-repo/environments/staging/providers.tf
+++ infra-repo/environments/staging/providers.tf
@@ -8,21 +8,12 @@ terraform {
     }
   }
 
-
-
-
-  backend "s3" {
-    bucket = "devops-project-01-tfstate-q919ah"
-    key = "devops-project-04/terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-    use_lockfile = true
-  }
 }
 
 
-  provider "aws" {
-    region = var.aws_region
+
+provider "aws" {
+  region = var.aws_region
   
 
   default_tags {
