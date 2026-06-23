b94189ddea40c89933075fea0aa598b5730d872c
Mayank-Bhawsar <mayankbhawsar1@gmail.com>
2026-06-23 20:14:15 +0000

    feat: upgrade terraform to v1.8.0 and implement native s3 state locking

--- infra-repo/environments/staging/providers.tf
+++ infra-repo/environments/staging/providers.tf
@@ -15,7 +15,6 @@ terraform {
     bucket = "devops-project-01-tfstate-q919ah"
     key = "devops-project-04/terraform.tfstate"
     region = "ap-south-1"
-    #dynamodb_table = "devops-project-01-tflocks"
     encrypt = true
     use_lockfile = true
   }
