939590db0be3c8388d29620cf56e0a8ce678e164
Mayank-Bhawsar <mayankbhawsar1@gmail.com>
2026-06-28 17:14:54 +0000

    fixed the type for the same unique random id again

--- app-repo/backend/app/main.py
+++ app-repo/backend/app/main.py
@@ -13,7 +13,7 @@ from app.core.config import settings
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     tag = route.tags[0] if route.tags else "default"
-    return f"{route.tags[0]}-{route.name}"
+    return f"{tag}-{route.name}"
 
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
